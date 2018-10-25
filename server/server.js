68e9f2fad539530772f967530df08530fe39279f
mehrotrarachit1113 <rachit.mehrotra1113@gmail.com>
2018-10-25 23:01:06 +0530

    POST API with test suite in mocha

--- server/server.js
+++ server/server.js
@@ -1,69 +1,29 @@
-const mongoose = require('mongoose')
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1/27017/trial')
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var trial = mongoose.model('trial' , {
-    text : {
-        type : String,
-        required : true,
-        minLength : 1,
-        trim : true
-    },
-    completed : {
-        type : Boolean,
-        default : false
-    },
-    completedAt : {
-        type : Number,
-        default : null
-    }
+var app = express();
+
+var PORT = process.env.PORT || 3000
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-//Creating the instances of the schema
-var firstTry = new trial({
-    text : 'Test1',
-    completed : 'True',
-    completedAt : 1
-})
+app.listen(PORT, () => {
+  console.log(`Started on port ${PORT}`);
+});
 
-var secondTry = new trial({
-    text : 'Test2',
-    completed : 'true',
-    completedAt : 2 
-})
-
-//Saving those instances in the db and pritning them with the implementation of promise
-  secondTry.save().then((doc) => {
-    console.log(doc)
-},(e) =>{
-    console.log(e)
-})
-
-firstTry.save().then((doc) => {
-    console.log('Saved ' , doc)
-    },(e) => {
-        console.log(e)
-    })
-
-// Another schema 
-
-var user = mongoose.model('user',{
-    emailId : String,
-    minLength : 1,
-    trim : true,
-    require : true
-})
-
-//Creating the instance of the user Schema
-var userFirstCreation = new user({
-    emailId : 'test@yopmail.com'
-})
-
-
-//Saving the data about the user
-userFirstCreation.save().then((result) =>{
-    console.log('Done')
-},(e) => {
-    console.log('Not Done')
-})
+module.exports = {app}
